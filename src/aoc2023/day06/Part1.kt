751a78a14f21a3da4726f0df899a04e978458b8f
Linden <linden.quan@resmed.com>
2023-12-06 15:24:03 -0400

    Day-06

---
+++ src/aoc2023/day06/Part1.kt
@@ -0,0 +1,53 @@
+package day06
+
+import process
+import readInput
+
+data class Race(val time: Long, val distance: Long) {
+    override fun toString(): String {
+        return "{time: $time, distance: $distance}"
+    }
+}
+
+
+fun main() {
+    process(readInput("aoc2023/day06/input.txt"), ::solvePart1)
+}
+
+fun solvePart1(inputTxt: List<String>) {
+    val input = getInput(inputTxt)
+    val output = input.map {
+        getNumberOfWinWays(it)
+    }.reduce { acc, unit -> acc * unit }
+
+    println(output)
+}
+
+fun getNumberOfWinWays(race: Race): Long {
+    var sum = 0L
+    for (i in 0..race.time) {
+        val distance = i * (race.time - i)
+        if (distance > race.distance) sum++
+    }
+
+    return sum
+}
+
+fun getInput(inputTxt: List<String>): MutableList<Race> {
+    val times = getNumberList(inputTxt[0])
+    val distances = getNumberList(inputTxt[1])
+    val input = mutableListOf<Race>()
+    for (i in times.indices) {
+        input.add(Race(times[i], distances[i]))
+    }
+
+    return input
+}
+
+fun getNumberList(s: String): List<Long> {
+    return s.substringAfter(":")
+        .split(Regex("\\s+"))
+        .filter { it.length > 0 }
+        .map { it.toLong() }
+}
+
---
+++ src/aoc2023/day06/Part2.kt
@@ -0,0 +1,23 @@
+package day06
+
+import process
+import readInput
+
+fun main() {
+    process(readInput("aoc2023/day06/input.txt"), ::solvePart2)
+}
+
+fun solvePart2(inputText: List<String>) {
+    val input = getInputForPart2(inputText)
+    println(getNumberOfWinWays(input))
+}
+
+fun getInputForPart2(inputText: List<String>): Race {
+    val time = getNumber(inputText[0])
+    val distance = getNumber(inputText[1])
+    return Race(time, distance)
+}
+
+fun getNumber(s: String): Long {
+   return s.substringAfter(":").replace(" ", "").toLong()
+}
