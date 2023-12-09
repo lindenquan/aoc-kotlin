02f85ce02d572e9dcdf9cd8d6758fc2e0cd9fd5a
Linden <linden.quan@resmed.com>
2023-12-08 21:22:02 -0400

    Day-08

---
+++ src/aoc2023/day08/Part1.kt
@@ -0,0 +1,46 @@
+package day08
+
+import process
+import readInput
+
+data class Input(
+    val steps: List<Int>, val map: Map<String, List<String>>
+)
+
+fun main() {
+    process(readInput("aoc2023/day08/input.txt"), ::solvePart1)
+}
+
+fun solvePart1(inputText: List<String>) {
+    val input = parseInputText(inputText)
+    println(calcSteps(input, "AAA", Regex("ZZZ")))
+}
+
+fun calcSteps(input: Input, startLocation: String, destination: Regex): Long {
+    val stepSize = input.steps.size
+    var steps = 0
+    var location = startLocation
+    while (true) {
+        val direction = input.steps[steps % stepSize]
+        location = input.map[location]!![direction]
+        steps++
+        if (destination.matches(location)) {
+            break
+        }
+    }
+
+    return steps.toLong()
+}
+
+fun parseInputText(inputText: List<String>): Input {
+    val steps = inputText[0].map { if (it == 'R') 1 else 0 }
+    val map = mutableMapOf<String, List<String>>()
+
+    inputText.drop(2).forEach {
+        val key = it.substring(0, 3)
+        val value = listOf(it.substring(7, 10), it.substring(12, 15))
+        map[key] = value
+    }
+
+    return Input(steps, map)
+}
---
+++ src/aoc2023/day08/Part2.kt
@@ -0,0 +1,22 @@
+package day08
+
+import process
+import readInput
+
+fun main() {
+    process(readInput("aoc2023/day08/input.txt"), ::solvePart2)
+}
+
+fun solvePart2(inputText: List<String>) {
+    val input = parseInputText(inputText)
+    val allZSteps = input.map.keys.filter {
+        it.endsWith("A")
+    }.map { calcSteps(input, it, Regex("..Z")) }
+
+    var steps = 1L
+    for (l in allZSteps) steps = leastCommonMultiple(steps, l)
+    println(steps)
+}
+
+tailrec fun greatestCommonDivisor(x: Long, y: Long): Long = if (y == 0L) x else greatestCommonDivisor(y, x % y)
+fun leastCommonMultiple(x: Long, y: Long) = x * y / greatestCommonDivisor(x, y)
